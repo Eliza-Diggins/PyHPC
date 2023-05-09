f34549f7703dab7f135145eae087b5623c7c9fec
Eliza Diggins (Linux) <u1281896@utah.edu>
2023-05-09 09:02:03 -0600

    configuration system is functional.

---
+++ PyHPC_Core/configuration.py
@@ -0,0 +1,65 @@
+"""
+        Configuration Management for PyCS Project
+                Written by: Eliza Diggins
+                -- Last Check: 12/16 --
+"""
+import os
+import pathlib as pt
+import toml as tml
+from colorama import Fore
+import cmocean as cmo
+from matplotlib.pyplot import cm
+import json
+from types import SimpleNamespace
+# --|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--#
+# ------------------------------------------------------ Variables ------------------------------------------------------#
+# --|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--#
+with open(os.path.join(pt.Path(__file__).parents[1],"bin","local","install.tkc")) as file:
+    ### Reading the CONFIG file from the installation ticket.
+    ticket_info = SimpleNamespace(**json.load(file))
+
+# Setting up the debug strings #
+_location = "PyHPC_Core"
+_filename = pt.Path(__file__).name.replace(".py", "")
+_dbg_string = "%s:%s" % (_location, _filename)
+
+
+# --|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--#
+# -------------------------------------------------------FUNCTIONS ------------------------------------------------------#
+# --|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--#
+def read_config() -> dict:
+    """
+    Grabbing the configuration system from the configuration file path.
+    :return: The configuration dictionary
+    """
+    ### reading the TOML string ###
+    config_dict = tml.load(os.path.join(ticket_info.installation_location,"bin","configs","CONFIG.config"))
+
+    #
+    #       Post Processing...
+    #
+
+    ## Manging colormaps ##
+    _fdbg_string = _dbg_string + Fore.LIGHTMAGENTA_EX + ":read_config:" + Fore.WHITE
+    for key in config_dict["Visualization"]["Colormaps"]:
+        try:
+            config_dict["Visualization"]["Colormaps"][key] = cm.get_cmap(config_dict["Visualization"]["Colormaps"][key])
+        except ValueError:  # The colormap doesn't exist
+            raise ValueError("%s Configuration key %s failed. %s is not a colormap." % (
+            _fdbg_string, key, config_dict["Visualization"]["Colormaps"][key]))
+
+    return config_dict
+
+
+# --|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--#
+# -------------------------------------------------------  MAIN  --------------------------------------------------------#
+# --|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--#
+if __name__ == '__main__':
+    CONFIG = read_config()
+    print(CONFIG)
+# --|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--#
+# -------------------------------------------------------  NOTES  -------------------------------------------------------#
+# --|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--#
+"""
+NOTES:
+"""
