10c70fd548d2515b1212675a481d3f3c5a47f580
Eliza Diggins (Linux) <u1281896@utah.edu>
2023-06-14 08:07:07 -0600

    added a new way of deleting things from the temp files.

--- PyHPC/bin/clear_temp.sh
+++ PyHPC/bin/clear_temp.sh
@@ -1,5 +1,5 @@
 # This simply clears the .tmp folder of any crap left in it.
 sleep 5 # Give the system enough time to rest
-setenv ROOTDIR = `dirname $0`
+setenv ROOTDIR  `dirname $0`
 setenv SCRIPT_DIR `cd $ROOTDIR && pwd`
 rm -rf $SCRIPT_DIR/.tmp/*
