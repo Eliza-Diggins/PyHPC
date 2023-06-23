9f6b3e60152b261821880aa627c005ecd471f4c1
Nathan Diggins <u1281896@utah.edu>
2023-06-23 09:28:18 -0600

    adding automated testing to github.

---
+++ tests/testGeneral.py
@@ -0,0 +1,49 @@
+"""
+Testing PyHPC
+=============
+
+The ``PyHPC`` module relies on a system of unit-tests to establish errors in the development process. They are documented
+below
+"""
+import json
+import logging
+import os
+import pathlib as pt
+import unittest
+
+
+class TestCore(unittest.TestCase):
+    with open(os.path.join(pt.Path(__file__).parents[0], "pytest_data.json")) as f:
+        test_data = json.load(f)["TestCore"]
+
+    def test_configuration(self):
+        """Tests the ``PyHPC.PyHPC_core.configuration`` module."""
+        #  Loading Modules
+        # ----------------------------------------------------------------------------------------------------------------- #
+        try:
+            from PyHPC.PyHPC_Core.configuration import read_config
+        except ImportError:
+            raise AssertionError("Couldn't import PyHPC.PyHPC_Core.configuration")
+
+        CONFIG = read_config()
+
+        assert isinstance(CONFIG, dict)
+
+    def test_logging(self):
+        """Tests the ``PyHPC.PyHPC_core.log`` module."""
+        #  Loading Modules
+        # ----------------------------------------------------------------------------------------------------------------- #
+        try:
+            from PyHPC.PyHPC_Core.log import configure_logging
+        except ImportError:
+            raise AssertionError("Failed to import PyHPC.PyHPC_Core.log")
+
+        configure_logging(__file__)
+
+        for id, dat in enumerate(zip(self.test_data["test_logging"]["loggers"],
+                                     self.test_data["test_logging"]["lengths"])):
+            logger, length = dat
+            with self.subTest(logger, i=id):
+                log = logging.getLogger(logger)
+
+                assert len(log.handlers) == length
