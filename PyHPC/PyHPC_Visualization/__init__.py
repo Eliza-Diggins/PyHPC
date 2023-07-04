35d5b0b580896746ad73a0fb79f3b015782e98bd
Nathan Diggins <u1281896@utah.edu>
2023-07-03 22:01:18 -0600

    Working on image production.

--- PyHPC/PyHPC_Visualization/__init__.py
+++ PyHPC/PyHPC_Visualization/__init__.py
@@ -1,17 +1,161 @@
 """
-========================
-Visualizations in PyHPC
-========================
+==============================
+Producing Images in ``PyHPC``
+==============================
+The ``PyHPC`` system uses the ``yt`` project as its backbone for analysis, and therefore plots through ``yt`` as well.
+There are several included plotting systems for various purposes and a catch-all strategy for batches of plots that need
+to be generated. In some advanced cases, ``yt`` may need to be used in isolation or in conjunction with ``PyHPC`` because
+automated systems are insufficient to meet the user's need.
 
-Introduction
-------------
+Automated Plotting
+------------------
+Because plotting is so important to the analysis system, but is rather tedious, especially on ``CHPC``, we have included
+a system for generating relatively simple plots for what are called ``pdirectives``. A ``pdirective`` is a file with
+extension ``.pd``, which contains instructions on how the system should plot a given set of data. These can then
+be read by ``PyHPC`` and be used either as an animation task or as a single plot.
 
-Notes
------
+Pdirectives
+^^^^^^^^^^^
+the ``.pd`` files should be formatted as follows:
 
-Developer Description
----------------------
-The core structure of the ``yt`` project's plotting is built on the ``matplotlib`` plotting library. In this case,
-the user may choose to use any of the implemented ``plotting`` routines described; and for each, the plot may
-be arbitrarily attached to an ``axes`` object to render it in a more complex plot.
+.. code-block:: yaml
+
+    information: |
+        This is never read and can include any important information about the plot.
+
+    #Figure is the core of the Pdirective and contains all of the data
+    #corresponding to the intended figure including its subplots.
+
+    Figure:
+        grid:
+            # The grid can be used to specify the gridspec layout desired.
+            - [1,1,2,2]
+            - [1,1,2,2]
+        Subplots:
+
+        #Subplots are the most important part of the Pdirective. Each one
+        #contains the function headers that correspond to the necessary functions
+        #to actually produce the visual.
+        # !Subplots are indexed by number!
+        # ! Each subplot must have "Functions" and "Parameters".
+        #
+            1:
+                Functions:
+                    # Each function in Function should be named by its correct reference (in the code). To use
+                    # yt.ProjectionPlot, you title it yt.ProjectionPlot as the key in the entry.
+                    #
+                    #    ! If you need to re-use a function in a SINGLE subplot, label it (N)function
+                    #    ! where N is the index.
+                    #
+                    # Each function specified must have entries: args and kwargs. These must correspond to those
+                    # recognized in the program itself. See the section below on ease of use for more detailed
+                    # information on this.
+                    #
+                    function1:
+                        args: ["x","y","z"]
+                        kwargs:
+                            "color": "red"
+                    function2:
+                        args: [":ds",":f"]
+                        kwargs:
+                            "cmap":"Blues"
+                Parameters:
+                    # The Parameters are those things which we wish to constrain about the subplot object itself.
+                    # Each Parameter must have a command ".something()", its args, and its kwargs. These are
+                    # run after each other plotting procedure has been finished and are done in order.
+                    .set_xlim:
+                        args: [[0,1]]
+                        kwargs: {}
+            2:
+                Functions:
+                    function1:
+                        args: ["x","y","z"]
+                        kwargs:
+                            "color": "red"
+                Parameters: {}
+        Parameters:
+            # These parameters are the same as those described above for subplots; however, this time they apply
+            # specifically to the figure object instead.
+            .set_size_inches:
+                args: [[10,10]]
+                kwargs: {}
+    PostProcessing:
+        # The post processing part of the directive can be excluded if unwanted. With this, you can change the default
+        # behavior after execution. There are two settings: post process, which is a pure python script to be executed
+        # after the course of the computation is completed.
+        #
+        # !! WARNING: this uses the exec() function. Because there are no security threats, this fault will be
+        #             permitted to persist; however, user idiocy could lead to unintended consequences for the
+        #              system.
+        #
+        # The second option in post processing is the output field, which can specify a save location for the finished
+        # plot.
+        #
+        output: None
+        script: |
+            pass
+
+Advanced User Settings
+^^^^^^^^^^^^^^^^^^^^^^
+There are a few further details to describe in the ``pdirective`` system. The foremost of these is the use of wildcards.
+
+- A ``wildcard`` variable in a ``.pd`` file is an ``arg, kwarg`` for a function which is denoted ``%{name}%``. When these
+  are placed in a ``.pd`` file, the pre-processor will automatically seek out the corresponding ``name`` kwarg in the
+  function producing the plot and filter that data in. This is most useful for filtering in the data you wish to plot.
+- What if I want to add a colorbar or other secondary structure?
+    This is intended as a simple / user-friendly plotting system; however, in ``PyHPC``, any plotting systems will only
+    return the corresponding ``PlotDirective``, which can be manipulated further by direct interaction with ``matplotlib``.
+- If the system includes argument sorting, look at the documentation below for instruction.
+
+Defaults
+^^^^^^^^
+The intention behind the ``PlotDirective`` construct is not to allow the user to interact easily with simple functions
+like ``plt.plot`` or ``plt.imshow``, but instead to allow a system for interacting with **user defined** plotting systems
+which may include many different components.
+
+For example, lets say that ``foo(x,y)`` generates a complex plot which includes a colorbar and some annotations as well as
+a legend. The user might not want to have to provide **all** of those ``kwargs`` to their ``pdirective`` file. This is where
+the ``master_directive.pd`` file comes to the rescue. This is a special file in the ``PyHPC`` system and can be used to
+generate pre-written settings for different components. The format is as follows:
+
+.. code-block:: yaml
+
+    Information: |
+        This is the master directive for the PyHPC system (mock up)
+    Functions:
+        # Functions
+        # ---------
+        #   These are all of the implemented functions for plotting. Here, we would include an entry for foo(x,y) as
+        # discussed above. In this case, the function takes only ``kwargs`` and not args, but the ``kwargs`` can
+        # take on a variety of roles.
+        foo:
+            kwargs:
+                color: "red"
+                linewidth: 3
+    Structures:
+        # Structures
+        # ----------
+        # Not all of the work we want to do while plotting requires interaction with files. In some cases, we have
+        # to interact with structures / objects as well. In this case, we specify the structural parameters as follows:
+        #
+        # 1. (args,kwargs): These are passed to a self-identified ``initialization_function``, where they may be used
+        #   to generate the object of importance. Please be aware that the person writing the custom script must dictate
+        #   the proper proceedure for different entry types.
+        #
+        # 2. commands:
+        #   Include direct commands with their own ``kwargs`` and ``args`` after the loading process.
+        Figure:
+            args: []
+            kwargs: {}
+            commands: {}
+
+In this case, **every time** the structure / function described in the ``master_directive`` is used in a ``.pd`` file,
+the defaults will be auto-populated unless overridden.
+
+.. todo::
+    There should be an implementation option by which to avoid having persistent defaults if unwanted.
+
+.. attention::
+    In the case of a complex function, it is advised that included kwargs are categorized so that the endpoint function
+    can easily differentiate those kwargs corresponding to different structures in the plotting system.
 """
---
+++ PyHPC/PyHPC_Visualization/plot.py
@@ -0,0 +1,285 @@
+"""
+The core module of the ``PyHPC_Visualization`` module.
+"""
+import logging
+import os
+import pathlib as pt
+import warnings
+
+import yaml
+import numpy as np
+from PyHPC.PyHPC_Core.configuration import read_config
+from PyHPC.PyHPC_Core.errors import PyHPC_Error
+import matplotlib.pyplot as plt
+import uplots
+# --|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--#
+# ------------------------------------------------------ Setup ----------------------------------------------------------#
+# --|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--#
+_location = "PyHPC_Visualization"
+_filename = pt.Path(__file__).name.replace(".py", "")
+_dbg_string = "%s:%s:" % (_location, _filename)
+CONFIG = read_config()
+modlog = logging.getLogger(__name__)
+
+# - managing warnings -#
+if not CONFIG["System"]["Logging"]["warnings"]:
+    warnings.filterwarnings('ignore')
+
+with open(os.path.join(pt.Path(__file__).parents[1], "bin", "lib", "img", "master_directive.yaml")) as file:
+    _mdir = yaml.load(file, yaml.FullLoader)
+
+
+class PlotDirective:
+    """
+    The ``PlotDirective`` class which is an object wrapper for ``.pd`` directive files.
+
+    Parameters
+    ----------
+    directive_loc: str
+        The ``str`` path to the file containing the ``directive`` object.
+    Attributes
+    ----------
+
+    Notes
+    -----
+
+    The format of the underlying file must meet the standards described in the documentation on plotting. To reiterate,
+
+    .. code-block:: yaml
+
+        information: |
+            This is never read and can include any important information about the plot.
+
+        #Figure is the core of the Pdirective and contains all of the data
+        #corresponding to the intended figure including its subplots.
+
+        Figure:
+            grid:
+                # The grid can be used to specify the gridspec layout desired.
+                - [1,1,2,2]
+                - [1,1,2,2]
+            Subplots:
+
+            #Subplots are the most important part of the Pdirective. Each one
+            #contains the function headers that correspond to the necessary functions
+            #to actually produce the visual.
+            # !Subplots are indexed by number!
+            # ! Each subplot must have "Functions" and "Parameters".
+            #
+                1:
+                    Functions:
+                        # Each function in Function should be named by its correct reference (in the code). To use
+                        # yt.ProjectionPlot, you title it yt.ProjectionPlot as the key in the entry.
+                        #
+                        #    ! If you need to re-use a function in a SINGLE subplot, label it (N)function
+                        #    ! where N is the index.
+                        #
+                        # Each function specified must have entries: args and kwargs. These must correspond to those
+                        # recognized in the program itself. See the section below on ease of use for more detailed
+                        # information on this.
+                        #
+                        function1:
+                            args: ["x","y","z"]
+                            kwargs:
+                                "color": "red"
+                        function2:
+                            args: [":ds",":f"]
+                            kwargs:
+                                "cmap":"Blues"
+                    Parameters:
+                        # The Parameters are those things which we wish to constrain about the subplot object itself.
+                        # Each Parameter must have a command ".something()", its args, and its kwargs. These are
+                        # run after each other plotting procedure has been finished and are done in order.
+                        .set_xlim:
+                            args: [[0,1]]
+                            kwargs: {}
+                2:
+                    Functions:
+                        function1:
+                            args: ["x","y","z"]
+                            kwargs:
+                                "color": "red"
+                    Parameters: {}
+            Parameters:
+                # These parameters are the same as those described above for subplots; however, this time they apply
+                # specifically to the figure object instead.
+                .set_size_inches:
+                    args: [[10,10]]
+                    kwargs: {}
+        PostProcessing:
+            # The post processing part of the directive can be excluded if unwanted. With this, you can change the default
+            # behavior after execution. There are two settings: post process, which is a pure python script to be executed
+            # after the course of the computation is completed.
+            #
+            # !! WARNING: this uses the exec() function. Because there are no security threats, this fault will be
+            #             permitted to persist; however, user idiocy could lead to unintended consequences for the
+            #              system.
+            #
+            # The second option in post processing is the output field, which can specify a save location for the finished
+            # plot.
+            #
+            output: None
+            script: |
+                pass
+    """
+    def __init__(self, directive_loc):
+        #  Logging
+        # ----------------------------------------------------------------------------------------------------------------- #
+        modlog.debug("Loading PlotDirective at %s." % directive_loc)
+
+        #  Attempting to load
+        # ----------------------------------------------------------------------------------------------------------------- #
+        try:
+            with open(directive_loc, "r") as file:
+                self._raw = yaml.load(file, yaml.FullLoader)
+        except FileNotFoundError:
+            raise PyHPC_Error("Failed to initialize PlotDirective at %s due to missing file." % directive_loc)
+
+        #  Parsing and generating
+        # ----------------------------------------------------------------------------------------------------------------- #
+        modlog.debug("Generating the figure...")
+        #: The figure connected to the ``directive``.
+        self.figure = plt.figure()
+
+        # - Setting figure properties -#
+        self._assert_figure_structure()
+
+        # - managing the figure behavior -#
+        _shape = np.array(self._raw['Figure']["grid"]).shape
+        gs = plt.GridSpec(**{k: v for k, v in zip(["ncols", "nrows"], _shape)})
+
+        # - adding the figures -#
+        #: subplots connected with the directive. Indexed by integers.
+        self.subplots = {}
+        keys = [i for i in sorted([int(k) for k in list(set(list(np.array(self._raw["Figure"]["grid"]).ravel())))])]
+        for key in keys:
+            matches = np.array(np.where((np.array(self._raw["Figure"]["grid"])) == key))
+            x_range, y_range = slice(np.amin(matches[0, :]), np.amax(matches[0, :]) + 1, 1), slice(
+                np.amin(matches[1, :]), np.amax(matches[1, :]) + 1, 1)
+
+            self.subplots[key] = self.figure.add_subplot(gs[y_range, x_range])
+            self._assert_subplot_structure(key)
+
+        #  Managing Functions
+        # ----------------------------------------------------------------------------------------------------------------- #
+        #: tracks the initialization errors.
+        _errors = []
+
+        #: functions associated with the plots. ``{subplot_id: {f1: {},f2:{},...}``.
+        self.functions = {k: {} for k in self.subplots}  #: initializing the self.functions dictionary.
+
+        for subplot, subplot_data in self._raw["Figure"]["Subplots"].items():
+            modlog.debug("initializing subplot %s." % subplot)
+            for function_key, function_val in subplot_data["Functions"].items():
+                # - Is the function actually in the master directive? - #
+                if not function_key in _mdir["Functions"]:
+                    _errors.append(
+                        {"type": "function", "name": function_key, "msg": "Failed to find in master_directive"})
+                    continue
+                else:
+                    pass
+            # - Can I add the function to the return functions? - #
+
+            if "." in function_key:
+                function_root = function_key.split(".")[0]
+                function_name = function_key.replace(function_root + ".", "")
+            else:
+                function_root = function_key
+                function_name = ""
+
+            if function_key[0] == "(":
+                function_root = function_root.split(")")[1]
+
+            # - Adding kwargs - #
+            kwargs = {**{k: v for k, v in _mdir["Functions"][function_key]["kwargs"].items() if
+                         k not in function_val["kwargs"]}, **function_val["kwargs"]}
+
+            try:
+                if len(function_name):
+                    self.functions[subplot][getattr(globals()[function_root], function_name)] = {
+                        "args": function_val["args"], "kwargs": kwargs}
+
+                else:
+                    self.functions[subplot][globals()[function_root]] = {"args": function_val["args"], "kwargs": kwargs}
+
+            except KeyError as err:
+                _errors.append(
+                    {"type": "function", "name": function_key,
+                     "msg" : "Failed to find function in globals: %s." % err.__repr__()})
+
+    # -------------------------------------------------------------------------------------------------------------------- #
+    # Methods ============================================================================================================ #
+    # -------------------------------------------------------------------------------------------------------------------- #
+    def __str__(self):
+        return "PlotDirective Object - %s funcs" % (len(self.functions))
+
+    def __repr__(self):
+        return "PlotDirective Object - %s" % (self._raw)
+
+    def _assert_figure_structure(self):
+        for method,vals in self._raw["Figure"]["Parameters"]:
+            _args,_kwargs = vals["args"],vals["kwargs"]
+
+            try:
+                getattr(self.figure,method)(*_args,**_kwargs)
+            except AttributeError:
+                modlog.warning("Failed to identify figure attribute %s."%method)
+
+    def _assert_subplot_structure(self,subplot):
+        for method,vals in self._raw["Figure"]["Subplots"][subplot]["Parameters"]:
+            _args,_kwargs = vals["args"],vals["kwargs"]
+
+            try:
+                getattr(self.subplots[subplot],method)(*_args,**_kwargs)
+            except AttributeError:
+                modlog.warning("Failed to identify subplot attribute %s."%method)
+
+# -------------------------------------------------------------------------------------------------------------------- #
+#  Functions ========================================================================================================= #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+def generate_image(image_directive, **kwargs):
+    """
+    Generates the image based on the directive ``image_directive``. The data must be provided with appropriate keys
+    through the ``kwargs``.
+
+    .. attention::
+        For further information on ``PlotDirective`` objects and the entire plotting process in ``PyHPC``, see documentation
+        for ``PyHPC.PyHPC_Visualization``.
+
+    Parameters
+    ----------
+    image_directive: PlotDirective
+        The ``PlotDirective`` object corresponding to the plotting routine.
+    kwargs: dict
+        The dictionary containing the information on data sources. For any wildcard value in ``kwargs`` or ``args`` to
+        the ``directive``, an associated data specifier must be included in args.
+
+
+    Returns
+    -------
+    None
+
+    """
+    # Logging and Setup
+    # ----------------------------------------------------------------------------------------------------------------- #
+    modlog.debug("Generating image for directive %s.")
+    #  Reading information from the directive.
+    # ----------------------------------------------------------------------------------------------------------------- #
+    for subplot_id, subplot in image_directive.subplots.items():
+        for func, vals in image_directive.functions[subplot_id].items():
+            kwargs['a'] = subplot
+            _args = [i if (str(i)[0] != "%") else kwargs[str(i).replace("%", "")] for i in list(vals["args"])]
+
+            _kwargs = {k: (v if str(v)[0] != "%" else kwargs[str(v).replace("%", "")]) for k, v
+                       in vals["kwargs"].items()}
+            func(*_args, **_kwargs, axes=subplot)
+
+    return image_directive.figure
+
+# -------------------------------------------------------------------------------------------------------------------- #
+#      =============================================================================================================== #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    pass
